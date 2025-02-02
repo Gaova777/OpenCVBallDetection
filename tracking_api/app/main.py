4b8e261337226ccef3dcda43fd3c3bfa44b58355
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2025-02-02 18:28:35 -0500

    FixWordOnFileNameVideo

--- tracking_api/app/main.py
+++ tracking_api/app/main.py
@@ -3,25 +3,17 @@ from video_tracking import process_video
 import os
 
 app = FastAPI()
-
-# Carpeta donde se almacenan los videos en el servidor
 VIDEO_FOLDER = "videos/"
-
-# Asegurar que la carpeta de videos exista
 os.makedirs(VIDEO_FOLDER, exist_ok=True)
 
 @app.get("/process_video/")
 async def process_local_video():
-    """
-    Procesa automáticamente un video almacenado en el servidor.
-    """
-    video_filename = "video5.mp4"  # Cambia esto si tienes otro video
+   
+    video_filename = "Video5.mp4"  
     video_path = os.path.join(VIDEO_FOLDER, video_filename)
 
     if not os.path.exists(video_path):
         return {"error": f"El archivo {video_filename} no existe en {VIDEO_FOLDER}"}
-
-    # Llamar a la función de procesamiento
     process_video(video_path)
     
     return {"message": f"Procesamiento de {video_filename} completado"}
